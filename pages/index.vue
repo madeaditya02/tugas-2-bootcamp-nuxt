ffbf7ae9a10893ab0b28e239c018541268bd36eb
MadeAditya02 <imadeaditya4@gmail.com>
2023-08-28 23:35:07 +0800

    Membuat repo

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="navbar">
+        <a href="" class="navbar-title">Problog</a>
+        <div class="navbar-nav">
+          <a href="#">Home</a>
+          <a href="#">Posts</a>
+          <a href="#">Bookmark</a>
+          <a href="#">About</a>
+        </div>
+        <a href="#" class="btn-blue login-btn">Login</a>
+      </div>
+      <div class="hero">
+        <div>
+          <h2>Place to write and share your stories and experiences</h2>
+          <a href="#" class="btn-blue">Start writing</a>
+        </div>
+        <img src="~assets/header.png" alt="">
+      </div>
+      <div class="data">
+        <div class="card">
+          <h2>100k+</h2>
+          <h4>Posts</h4>
+        </div>
+        <div class="card">
+          <h2>50k+</h2>
+          <h4>Writers</h4>
+        </div>
+        <div class="card">
+          <h2>300k+</h2>
+          <h4>Likes</h4>
+        </div>
+      </div>
+    </div>
+    <footer>
+      <a href="" class="navbar-title">Problog</a>
+      <div>
+        <div class="profile">
+          <div class="profile-info">
+            <h3>Email :</h3>
+            <a target="_blank" href="mailto:imadeaditya4@gmail.com">imadeaditya4@gmail.com</a>
+          </div>
+          <div class="profile-info">
+            <h3>Whatsapp :</h3>
+            <a target="_blank" href="https://wa.me/08973891362">08973891362</a>
+          </div>
+        </div>
+        <div class="profile">
+          <div class="profile-info">
+            <h3>Github :</h3>
+            <a target="_blank" href="https://github.com/madeaditya02">MadeAditya02</a>
+          </div>
+          <div class="profile-info">
+            <h3>Instagram :</h3>
+            <a target="_blank" href="https://instagram.com/mdadityaa02">@mdadityaa02</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
